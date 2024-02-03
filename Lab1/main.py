f9910a281d0033113358f18e065103d847f7d118
Tyler Seefeldt <tseefeldt@gmail.com>
2024-02-03 11:56:26 -0700

    Added whitespace to main in Lab 1 and added a Test directory

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,7 +1,9 @@
 import math
 
+
 def is_repetable(pattern, list):
     is_repeat = True
+
     for i in range(0, len(list), len(pattern)):
         sample = list[i:i+len(pattern)]
         if sample != pattern:
@@ -9,11 +11,13 @@ def is_repetable(pattern, list):
 
     return is_repeat
 
+
 def repeats(list):
     if list is None:
         return None
     elif len(list) == 1:
         return None
+        
     repeat = None
     
     for i in range(1, math.floor(len(list) / 2) + 1): 
