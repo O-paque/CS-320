6385ab62664b8e4758e283e2d4dd67ff63297c19
Tyler <tseefeldt@gmail.com>
2024-02-22 15:02:58 -0700

    Worked on Lab3

---
+++ Lab3/main.py
@@ -0,0 +1,5 @@
+import math
+
+
+def mergesort(mlist):
+    return 0
---
+++ Lab3/zyBook/main.py
@@ -0,0 +1,8 @@
+import math # optional and you can delete this line if not useful
+
+
+# subroutines if any, go here
+
+
+# fill in mergesort
+def mergesort(mlist):
