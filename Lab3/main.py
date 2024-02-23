ece94189adb8fc6e73cef45e198e96e7068f0377
Tyler Seefeldt <tseefeldt@gmail.com>
2024-02-22 18:24:14 -0700

    Finished Lab3

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,32 +1,35 @@
 import math
 
-def merge(front,back,merged):
+
+def merge(front, back):
     i = 0
     j = 0
+    merged = []
     
-    print("Merged before sort: ", merged)
-    
-    while i in range(len(merged)) and j in range(len(merged)):
+    while i in range(len(front)) and j in range(len(back)):
         if front[i] <= back[j]:
-            merged[i+j] = front[i]
+            merged.append(front[i]) 
             i += 1
         else:
-            merged[i+j] = back[j]
+            merged.append(back[j]) 
             j += 1
         
-    while i in range(len(merged)):
-        merged[i+j] = front[i]
+    while i in range(len(front)):
+        merged.append(front[i])
         i += 1
         
-    while j in range(len(merged)):
-        merged[i+j] = back[j]
+    while j in range(len(back)):
+        merged.append(back[j]) 
         j += 1
-    
-    print("Merged after sort: ", merged)
+
     return merged
 
 
 def mergesort(mlist):
+    if mlist is None:
+        return None
+    elif mlist == []:
+        return []
     
     if len(mlist) > 1:
         middle = math.floor(len(mlist) / 2)
@@ -35,27 +38,4 @@ def mergesort(mlist):
     else:
         return mlist
     
-    merge(frontList, backList, mlist)
-    return mlist
-    
-
-    
-
-testList1 = [0,8,4,9,2,6]
-merge1 = [2,4]
-merge2 = [1,3]
-merge3 = [0, 0, 0, 0]
-
-print(merge(merge1, merge2, merge3))
-
-testResponse1 = mergesort(testList1)
-
-print("Response 1: ", testResponse1)
-
-x = 5
-m = 0
-
-while m in range(x):
-    print(m)
-    m += 1
-
+    return merge(frontList, backList)
--- Testing/main.py
+++ Testing/main.py
@@ -1,1 +1,5 @@
 print("Hello World!")
+
+list1 = [2,3,4,5]
+
+print(list1[0])
