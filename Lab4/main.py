aeeeb8773ee8da6845a5932f249016a327d4301e
Tyler Seefeldt <tseefeldt@gmail.com>
2024-03-02 16:05:55 -0700

    Finished Lab 4

--- Lab4/main.py
+++ Lab4/main.py
@@ -28,162 +28,42 @@ def build_adj_list(torus, i, j):
     return adj
 
 
-def find_path(torus, path, i, j):
+def find_path(torus, i, j, dictionary):
+    if (i, j) in dictionary:
+        return dictionary[(i, j)]
     
     adj_list = build_adj_list(torus, i, j)
-    print("adj_list in find_path: ", adj_list)
     
-    longest = []
-    # temp = path  # Do I need a temp value in this function??? 
-    temp = []
-    temp.append(path)    
-    for i in adj_list:
-
-        print("I in find_path: ", i)
-        adj_x, adj_y = i
-        path_x, path_y = path[len(path) - 1]
-        adjacent = torus[adj_x][adj_y]
-        compare = torus[path_x][path_y]
+    longest = [(i, j)]
+    for k in adj_list:
+        adj_x, adj_y = k
         
-        if (adjacent > compare) and (adj_x, adj_y) not in path:
-            temp.append((adj_x, adj_y))
-            find_path(torus, temp, adj_x, adj_y)
-            
-        if len(temp) > len(longest):
-            longest = temp
-    # path = longest
+        if torus[adj_x][adj_y] > torus[i][j]:
+            path = find_path(torus, adj_x, adj_y, dictionary)
+            if len(path) + 1 > len(longest):
+                longest = [(i, j)] + path
+        
+    dictionary[(i, j)] = longest
     return longest
 
 
 def longest_path(torus):
-    if torus is None:
+    if torus is None or not torus or not torus[0]:
         return []
     
     rows = len(torus)
     cols = len(torus[0])
-    
-    if rows == 0 or cols == 0:
-        return []
+
     i, j = (0, 0)
     longest = []
     
     while i < rows:
         while j < cols:
-            path = [(i, j)]
-            # print(torus[i][j], end = " ")
-            temp = find_path(torus, path, i, j)
-            if len(temp) > len(longest):
-                longest = temp
+            path = find_path(torus, i, j, {})
+            if len(path) > len(longest):
+                longest = path
             j += 1
         i += 1
         j = 0
-        # print()
     
-    return longest
-
-
-# def find_path_2(torus, path, i, j):
-    
-#     adj_list = build_adj_list(torus, i, j)
-#     print("adj_list in find_path: ", adj_list)
-    
-#     smallest = adj_list[0]
-    
-#     for i in adj_list:
-#          adj_x, adj_y = i
-#          path_x, path_y = smallest
-#          temp = torus[adj_x][adj_y]
-#          compare = torus[path_x][path_y]
-#          print("Temp: ", temp,", compare: ", compare)
-#          if (temp < compare) and i not in path:
-#              smallest = (adj_x, adj_y)
-             
-#     print("Smallest: ", smallest)
-        
-#     if smallest not in path:
-#         path.append(smallest)
-#         adj_x, adj_y = smallest
-#         find_path_2(torus, path, adj_x, adj_y)
-
-        
-#     return path
-        
-
-print("test arrays")
-arr1 = [[0,9,8],
-        [3,5,1],
-        [4,2,6]]
-
-arr2 = [[0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]]
-
-print("test that array indexing works as expected")
-print(arr1)
-arr1[1][2] = 9
-print(arr1)
-arr1[1][2] = 1
-print(arr1)
-arr1[2][0] = 100
-print(arr1)
-arr1[2][0] = 4
-print(arr1)
-
-print("test being able to find the rows and columns of an unknown array/matrix")
-print(arr2)
-rows = len(arr2)
-cols = len(arr2[0])
-print("Rows in arr2: ", rows, "\nCols in arr2: ", cols)
-
-print("test making an 2D array of zeroes will work as expected")
-visited = [[0 for i in range(cols)] for j in range(rows)]
-print(visited)
-visited[1][3] += 1
-print(visited)
-
-print("test appending works on anything")
-test1 = []
-test1.append((0,0))
-test1.append(8)
-test1.append('abc')
-print(test1)
-if (0,0) in test1:
-    print("We found it!")
-
-print("test longest_path returns without errors")
-longest_path(arr1)
-
-print("test build_adj_list works properly")
-i, j = (1,1) # change this to any [0-2][0-2] index and check manually
-adj1 = build_adj_list(arr1, i, j)
-print()
-longest_path(arr1)
-print(adj1)
-print("Start: ", arr1[i][j])
-
-i, j = adj1[0]
-print("Left: ", arr1[i][j])
-
-i, j = adj1[1]
-print("Right: ", arr1[i][j])
-
-i, j = adj1[2]
-print("Top: ", arr1[i][j])
-
-i, j = adj1[3]
-print("Bottom: ", arr1[i][j])
-
-for i in adj1:
-    print("i: ", i)
-    x, y = i
-    print("x: ", x, ", y: ", y)
-    
-print("test find_path works properly")
-path1 = [(1,1)]
-path1 = find_path(arr1, path1, 1, 1)
-print(path1)
-
-# print("test find_path_2 works properly")
-# path1 = [(0,0)]
-# path1 = find_path_2(arr1, path1, 0, 0)
-# print(path1)
+    return longest if len(longest) > 1 else []
--- Testing/dfs.py
+++ Testing/dfs.py
@@ -2,50 +2,42 @@ def longest_path(torus):
     if torus is None or not torus or not torus[0]:
         return []
 
-    m, n = len(torus), len(torus[0])
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-    def is_valid(x, y, prev_value):
-        return 0 <= x < m and 0 <= y < n and torus[x][y] > prev_value
-
-    def dfs(x, y, memo):
-        print("new dfs")
-        if (x, y) in memo:
-            return memo[(x, y)]
-
-        max_path = [(x, y)]
-        current_value = torus[x][y]
-
-        for dx, dy in directions:
-            print("dx: ", dx, ", dy: ", dy)
-            new_x, new_y = (x + dx) % m, (y + dy) % n
-
-            if is_valid(new_x, new_y, current_value):
-                path = dfs(new_x, new_y, memo)
-                if len(path) + 1 > len(max_path):
-                    max_path = [(x, y)] + path
-
-        memo[(x, y)] = max_path
-        return max_path
+    rows, columns = len(torus), len(torus[0])
 
     result = []
-    for i in range(m):
-        for j in range(n):
-            path = dfs(i, j, {})
+    for i in range(rows):
+        for j in range(columns):
+            path = dfs(torus, i, j, {})
             if len(path) > len(result):
                 result = path
 
     return result if len(result) > 1 else []
 
-# Example usage:
-torus = [
-    [1, 2, 3],
-    [6, 5, 4],
-    [7, 8, 9]
-]
+def is_valid(x, y, rows, columns, torus, prev_value):
+    return 0 <= x < rows and 0 <= y < columns and torus[x][y] > prev_value
 
-result = longest_path(torus)
-print(result)
+def dfs(torus, x, y, memo):
+    rows, columns = len(torus), len(torus[0])
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    print("new dfs")
+    if (x, y) in memo:
+        return memo[(x, y)]
+
+    max_path = [(x, y)]
+    current_value = torus[x][y]
+
+    for dx, dy in directions:
+        print("dx: ", dx, ", dy: ", dy)
+        new_x, new_y = (x + dx) % rows, (y + dy) % columns
+
+        if is_valid(new_x, new_y, rows, columns, torus, current_value):
+            path = dfs(torus, new_x, new_y, memo)
+            if len(path) + 1 > len(max_path):
+                max_path = [(x, y)] + path
+
+    memo[(x, y)] = max_path
+    return max_path
+
 
 arr1 = [[0,9,8],
         [3,5,1],
