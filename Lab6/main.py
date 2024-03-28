bb4f8a4c97adff29269ed8229ce1cf0cc09d1cbf
Tyler <tseefeldt@gmail.com>
2024-03-28 12:24:33 -0600

    Added edge case tests to start of bfs function

--- Lab6/main.py
+++ Lab6/main.py
@@ -1,4 +1,15 @@
 from edgegraph import *
 
 def bfs(graph: GraphEL, start: VertexEL) -> list:
-    pass
+    if graph is None or start is None or start not in graph.vertices():
+        return []
+    
+    print("end of bfs")
+
+graph = GraphEL()
+vertex1 = VertexEL("1")
+vertex2 = VertexEL("2")
+
+graph.add_vertex(vertex1)
+
+bfs(graph, vertex1)
