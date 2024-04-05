7404dc2fdcfa8bba4900dac7a73719601e56dc74
Tyler <tseefeldt@gmail.com>
2024-04-05 14:46:06 -0600

    Completed Bellman-Ford algorithm with commented test code

--- Testing/main.py
+++ Testing/main.py
@@ -1,5 +1,20 @@
+import sys
+
 print("Hello World!")
 
 list1 = [2,3,4,5]
 
 print(list1[0])
+
+print(sys.maxsize)
+
+vertex_dict = {}
+
+vertex_dict[1] = [0, 1, 2, 3, 4]
+
+
+print(vertex_dict[1])
+
+vertex_dict[1] = sys.maxsize
+
+print(vertex_dict[1])
