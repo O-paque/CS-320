0f9d54b3e2b485ee89a103db3aaf626a33eb28bc
Tyler <tseefeldt@gmail.com>
2024-04-09 11:06:25 -0600

    Consolidated input validation to a function

--- Lab8/main.py
+++ Lab8/main.py
@@ -1,7 +1,17 @@
 
 
 def ga_tsp(initial_population, distances, generations):
+    if not valid_inputs(initial_population, distances, generations):
+        return None
+
+    return 0
+    
+
+def valid_inputs(initial_population, distances, generations):
     if initial_population is None \
             or distances is None \
-            or generations is None:
-        return None
+            or generations is None \
+            or generations <= 0:       
+        return False
+    
+    return True
