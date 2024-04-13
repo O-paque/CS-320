1330dfe818c99e26e5bd9b964b4c6fd1440d1797
Tyler Seefeldt <tseefeldt@gmail.com>
2024-04-13 11:42:48 -0600

    Added function descriptions, created next_gen() to determine the next generation's population

--- Lab8/main.py
+++ Lab8/main.py
@@ -1,16 +1,14 @@
 import random
 
 
+# Genetric Algorithm to solve a symmetric and complete TSP
 def ga_tsp(initial_population, distances, generations):
     if not valid_inputs(initial_population, distances, generations):
         return None
     
     p_size = len(initial_population)
-    
     current_population = initial_population
     
-    # print("Current population= ", current_population)
-    times_equal = 0
     for i in range (0, generations):
         child_list = []
         
@@ -24,20 +22,13 @@ def ga_tsp(initial_population, distances, generations):
                 
             child_list.append(crossover(parent_A, parent_B))
         
-        # print("Current: ", current_population)
-        # print("Children: ", child_list)
         
-        print("Generation: ", i)
-        for k in current_population:
-            for f in child_list:
-                if k == f:
-                    print("Match: ", k , " and ", f)
+        current_population = next_gen(current_population, child_list)
         
-
-    print("Times equal: ", times_equal)
     return (0,0)
     
 
+# Checks for input validations per assignment guidelines
 def valid_inputs(initial_population, distances, generations):
     if initial_population is None \
             or distances is None \
@@ -48,6 +39,7 @@ def valid_inputs(initial_population, distances, generations):
     return True
 
 
+# Takes part of a path from parents A and B and combines them into a child
 def crossover(A, B):
     split_index = random.randint(1, len(A) - 2)     #-2 to always include A & B
     child = A[:split_index]
@@ -64,8 +56,20 @@ def crossover(A, B):
     return child
         
 
+# Determines the next generation's population for the Genetic Algorithm
+# Parent selection was random in crossover so we choose the "best"
+# cost across both parents and children for next_gen
+def next_gen(current, children):
+    #remove duplicate paths, more likely in smaller solution spaces
+    for i in current:
+        for j in children:
+            if i == j:
+                children.remove(f)
+    
+    return 0
+    
+    
 
-        
             
     
     
