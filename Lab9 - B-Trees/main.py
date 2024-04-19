078a7560f441e778583b6a46d2f384320fb23f79
Tyler <tseefeldt@gmail.com>
2024-04-19 12:30:54 -0600

    Added needed function declarations for B-Tree operations

--- Lab9 - B-Trees/main.py
+++ Lab9 - B-Trees/main.py
@@ -2,7 +2,7 @@ class BTree:
     def __init__(self, keys=[], children=[], max_size=4):
         self._keys = keys
         self._children = children
-        self.max_size = 4
+        self.max_size = max_size
 
     def __repr__(self):
         return self.__str__()
@@ -29,4 +29,21 @@ class BTree:
         if self._children[0] is None:
             return None
         return self._children
+    
+    #Insert value into the tree. If value exists, insert() does nothing.
+    def insert(self, value):
+        pass
+    
+    #Delete the value from the tree if it exists.
+    def delete(self, value):
+        pass
+    
+    #Returns True if the value exists in the tree, otherwise return False
+    def contains(self, value):
+        pass
+    
+    #Return a tuple of the values in the tree in an in-order traversal
+    def traverse(self):
+        pass
+    
         
